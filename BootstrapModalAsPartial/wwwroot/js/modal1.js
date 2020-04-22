7230ed98628995d3c3ee22ac5f2e968f2be6c4b0
Edward Mays <azure-tdl@copperstarsystems.com>
2020-04-22 13:49:44 -0500

    Update modal1.js to get data from web service, update _Modal1Partial.cshtml to display a value

--- BootstrapModalAsPartial/wwwroot/js/modal1.js
+++ BootstrapModalAsPartial/wwwroot/js/modal1.js
@@ -1,6 +1,12 @@
 ﻿$(document).ready(function () {
-    $('#modal1').on('shown.bs.modal',
+    $('#modal1').on('show.bs.modal',
         function () {
             //$('#myInput').trigger('focus');
+            var url = 'https://jsonplaceholder.typicode.com/users';
+            $.get(url,
+                function(result) {
+                    var data = result[0];
+                    $('#name').val(data.name);
+                });
         });
 });
