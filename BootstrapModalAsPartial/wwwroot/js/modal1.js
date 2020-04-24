c73c2920b84103be68586ebcde6d9d5653496b60
Edward Mays <azure-tdl@copperstarsystems.com>
2020-04-24 14:13:46 -0500

    Fix modal dismissal when saving changes

--- BootstrapModalAsPartial/wwwroot/js/modal1.js
+++ BootstrapModalAsPartial/wwwroot/js/modal1.js
@@ -1,12 +1,25 @@
-﻿$(document).ready(function () {
-    $('#modal1').on('show.bs.modal',
-        function () {
-            //$('#myInput').trigger('focus');
-            var url = 'https://jsonplaceholder.typicode.com/users';
-            $.get(url,
-                function(result) {
-                    var data = result[0];
-                    $('#name').val(data.name);
+﻿$(document).ready(function() {
+    $("#modal1").on("show.bs.modal", getData);
+    $("#update-person-button").on("click",
+        function (event) {
+            var url = "https://jsonplaceholder.typicode.com/users";
+            var data = $("#personForm").serialize();
+            $.post(url,
+                data,
+                function (response) {
+                    console.log(response);
+                    $("#modal1").modal('toggle');
                 });
         });
 });
+
+function getData() {
+    var url = "https://jsonplaceholder.typicode.com/users";
+    $.get(url, mapData);
+}
+
+function mapData(result) {
+    var data = result[0];
+    $("#name").val(data.name);
+    $("#userName").val(data.username);
+}
